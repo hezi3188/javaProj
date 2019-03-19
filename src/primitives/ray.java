2020a1e88f4814f13d0bbfda66094f101fb897ac
Hezi <hezi3188@gmail.com>
2019-03-19 13:00:40 +0200

    vector and ray

--- src/primitives/ray.java
+++ src/primitives/ray.java
@@ -24,15 +24,30 @@ package primitives;
 
 
 
-public class ray {
+public class ray extends vector {
 	 pointD3 start;
-	 vector vec;
 
 	public ray(pointD3 strat, vector vec) {
 		super();
-		this.start = strat;
-		this.vec = vec;
+		this.start = new pointD3(strat);
 	}
+	public ray (ray r){
+		super(r);
+		this.start = new pointD3(r.getStart());
+	}
+	public ray(){
+		super();
+		start = new pointD3();
+	}
+
+	public pointD3 getStart() {
+		return new pointD3(start);
+	}
+
+	public void setStart(pointD3 start) {
+		this.start = new pointD3(start) ;
+	}
+
 	@Override
 	public String toString() {
 		return "ray [strat=" + start + ", vec=" + vec + "]";
--- src/primitives/vector.java
+++ src/primitives/vector.java
@@ -9,7 +9,6 @@ package primitives;
 
 // ***************** Getters/Setters ********************** //
 
-// ***************** Administration  ******************** //
 
 /**
  * Javadoc formatted documentation
@@ -27,12 +26,50 @@ package primitives;
 public class vector {
 	pointD3 point;
 
+	public vector() {
+		this.point = new pointD3();
+	}
+	public vector(vector vec){
+		this.point = new pointD3(vec.getPoint()) ;
+	}
 	public vector(pointD3 point) {
 		this.point = new pointD3(point) ;
+	}
+
+	public pointD3 getPoint() {
+		return new pointD3(point) ;
+	}
+
+	public void setPoint(pointD3 point) {
+		this.point = new pointD3(point);
 	}
 
 	@Override
 	public String toString() {
 		return "vector [point=" + point + "]";
 	}
+
+	// ***************** Administration  ******************** //
+	public vector add(vector vec){
+		return this;
+	}
+	public vector substract(vector vec){
+		return  this;
+	}
+	public vector mult(double scalar){
+		return  this;
+	}
+	public double dotProduct(vector vec){
+		return  3;
+	}
+	public double length (){
+		return  3;
+	}
+	public vector crossProduct(vector vec){
+		return  this;
+	}
+	public vector normalize(){
+		return  this;
+	}
+
 }
