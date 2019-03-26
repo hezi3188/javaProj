65687fbc4ff41abfafc3d6ce0d04b8c4b79f1a0b
avishai elia <avishome@gmail.com>
2019-03-26 12:12:20 +0200

    geometric

---
+++ src/primitives/geometries/Plane.java
@@ -0,0 +1,7 @@
+package geometries;
+import primitives;
+
+public class Plane {
+    protected pointD3 a;
+    protected ray cross;
+}
---
+++ src/primitives/geometries/Triangle.java
@@ -0,0 +1,8 @@
+package geometries;
+import primitives;
+
+public class Triangle {
+    protected pointD3 a;
+    protected pointD3 b;
+    protected pointD3 3;
+}
